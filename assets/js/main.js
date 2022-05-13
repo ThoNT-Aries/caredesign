6c834c82606e811597f09004466410332b11f2e3
vanhieu <vanhieuu99@gmail.com>
2022-05-13 15:51:26 +0700

    commit code

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,8 +110,9 @@
    */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
+    this.classList.toggle('bi-x')
+    
   })
 
   /**
